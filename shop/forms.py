766852f8d77ea6a73c88aa2c1a4d60a4f7a86f09
aziz <azizboukardougha.ab@gmail.com>
2024-10-28 01:35:48 +0100

    Wishlist/notification cruds done

--- shop/forms.py
+++ shop/forms.py
@@ -1,8 +1,25 @@
 from django import forms
 from .models import ReviewRating
+from .models import Notification
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = ReviewRating
         fields = ['review', 'rating']
+
+
+
+
+class NotificationForm(forms.ModelForm):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+
+
+
+class NotificationUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Notification
+        fields = ['message', 'target_price', 'notify_on_sale']
